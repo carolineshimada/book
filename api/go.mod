553035b9160ae5306b259b8cdb3381dde7db983a
carolineshimada <caroline_shimada@hotmail.com>
2023-12-01 15:37:37 +0100

    feat: add connection with database

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,6 @@ module api
 
 go 1.21.4
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- api/main.go
+++ api/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	router "api/src/Router"
+	"api/src/config"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Load()
+
+	fmt.Println((config.ConnectionString))
+
 	fmt.Print("Rodando Api")
 	r := router.Gerar()
-	log.Fatal(http.ListenAndServe(":5001", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprint(":%d", config.Port), r))
 }
